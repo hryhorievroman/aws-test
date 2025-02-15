4226af44bf6eacb51565d0380bb7c7680a74c562
Barburon <lamis.htc@gmail.com>
2025-02-14 23:48:42 -0600

    Initial commit

---
+++ src/main/java/com/taskmanagement/awstest/Controller.java
@@ -0,0 +1,15 @@
+package com.taskmanagement.awstest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+
+    @GetMapping
+    public String test() {
+        return "Hello World";
+    }
+}
